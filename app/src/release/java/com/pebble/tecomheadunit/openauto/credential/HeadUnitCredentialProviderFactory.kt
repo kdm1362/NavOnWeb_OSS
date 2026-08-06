830756d5070a463f83003ad385f759ced6fea822
kdm1362 <kdm1362@gmail.com>
2026-08-06 23:32:17 +0900

    Fix release Android Auto credential provisioning

--- app/src/release/java/com/pebble/tecomheadunit/openauto/credential/HeadUnitCredentialProviderFactory.kt
+++ app/src/release/java/com/pebble/tecomheadunit/openauto/credential/HeadUnitCredentialProviderFactory.kt
@@ -4,14 +4,14 @@
 package com.pebble.tecomheadunit.openauto.credential
 
 import android.content.Context
-import com.pebble.tecomheadunit.BuildConfig
 
 object HeadUnitCredentialProviderFactory {
     fun create(@Suppress("UNUSED_PARAMETER") context: Context): HeadUnitCredentialProvider =
         AndroidKeyStoreCredentialProvider(
-            trustPolicy = PinnedHeadUnitCredentialTrustPolicy.fromSha256PinLists(
-                leafPins = BuildConfig.AASDK_IDENTITY_LEAF_SHA256,
-                anchorPins = BuildConfig.AASDK_IDENTITY_ANCHOR_SHA256,
-            ),
+            requireHardwareBacked = false,
+            provisionIfMissing = true,
+            trustPolicy = HeadUnitCredentialTrustPolicy {
+                HeadUnitCredentialTrustDecision.TRUSTED
+            },
         )
 }
